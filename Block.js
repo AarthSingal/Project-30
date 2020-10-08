b92b261da22f73bd7fc1fd77b48b4acf733d7791
AarthSingal <70443223+AarthSingal@users.noreply.github.com>
2020-10-08 19:53:01 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,25 @@
+class Block extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.visibilty = 255;
+      this.image = loadImage("Block.png")
+    }
+    display(){
+      console.log(this.body.speed);
+      
+      if(this.body.speed <  3){
+        super.display();
+        console.log("no");
+      }else{
+        World.remove(world,this.body);
+        push();
+        this.visibilty = this.visibilty-5;
+        imageMode(CENTER);
+        tint(255,this.visibilty);
+        image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+        console.log(this.body.speed);
+        pop();
+      }
+    }
+};
+  
---
+++ sketch.js
@@ -0,0 +1,78 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var ground;
+var base;
+var block1,block2,block3,block4,block5,block6;
+var polygon , polygonImage;
+var sling;
+
+function preload(){
+  polygonImage = loadImage("hexagon.png");
+}
+
+function setup() {
+  createCanvas(1300,600);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground = new Ground(650,500,1300,20);
+
+  base = new Ground(1000,250,400,15);
+
+  block1 = new Block(900,200,80,80);
+  block2 = new Block(985,200,80,80);
+  block3 = new Block(1070,200,80,80);
+  block4 = new Block(942.5,120,80,80);
+  block5 = new Block(1027.5,120,80,80);
+  block6 = new Block(980,40,80,80);
+
+  
+  polygon = Bodies.circle(200,200,50,{friction : 1});
+  World.add(world,polygon);
+
+  sling = new Rope(polygon,200,100);
+
+  Engine.run(engine);
+
+
+}
+
+function draw() {
+  background("lightblue");  
+  
+  Engine.update(engine);
+  ground.display();
+
+  base.display();
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+
+  console.log(block1.body.speed);
+
+  imageMode(CENTER);
+  image(polygonImage,polygon.position.x,polygon.position.y,50,50);
+
+  sling.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(polygon,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+  sling.fly();
+}
+function keyPressed(){
+  if(keyCode === 32){
+      sling.attach(polygon);
+
+  }
+}
